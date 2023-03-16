b3bccddeec05e4a05f5abbc257ad903df5055986
Yue JIN <yuejin13@qq.com>
2023-03-16 20:24:25 +0800

    feat: add cypress example

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,7 +2,9 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
+    viewportWidth: 1280,
+    viewportHeight: 900,
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
-    baseUrl: 'http://localhost:4173'
-  }
+    baseUrl: 'http://localhost:4173',
+  },
 })
--- cypress/e2e/example.cy.ts
+++ cypress/e2e/example.cy.ts
@@ -3,6 +3,19 @@
 describe('My First Test', () => {
   it('visits the app root url', () => {
     cy.visit('/')
-    cy.contains('h1', 'You did it!')
+    cy.url().should('eq', 'http://localhost:4173/homepage')
+    cy.contains('p', 'Opinionated Starter Template')
+    cy.contains('label', "What's your name?")
+      .invoke('attr', 'for')
+      .then((id) => {
+        cy.get('#' + id)
+      })
+      .type('kingyue737')
+
+    cy.contains('button', 'Confirm').click()
+    cy.contains('div', 'Hi, kingyue737!')
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(5000)
+    cy.contains('div', 'Hi, kingyue737!').should('not.exist')
   })
 })
