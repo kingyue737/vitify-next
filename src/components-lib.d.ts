e517b8c990fdf849a1e26377dadc647e49444346
Yue JIN <yjin@nustarnuclear.com>
2023-05-15 11:12:20 +0800

    refactor: remove vuetify labs type shim

--- src/components-lib.d.ts
+++ src/components-lib.d.ts
@@ -5,6 +5,5 @@ export {}
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     VChart: typeof import('vue-echarts')['default']
-    VDataTable: typeof import('vuetify/labs/VDataTable')['VDataTable']
   }
 }
