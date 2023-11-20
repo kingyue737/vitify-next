ef0940d741abad207ff4b69212cb4de21f952788
Yue JIN <yjin@nustarnuclear.com>
2023-11-20 14:14:57 +0800

    fix: useToggle switch type

--- src/components/layout/AppBar.vue
+++ src/components/layout/AppBar.vue
@@ -22,7 +22,7 @@ const isDark = useDark({
     theme.global.name.value = dark ? 'dark' : 'light'
   },
 })
-const toggleDark = useToggle(isDark)
+const toggleDark = useToggle<true, false | null>(isDark)
 </script>
 
 <template>
