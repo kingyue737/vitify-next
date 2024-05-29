e9a1b6f33ac3bc9c6d77e375ed7bf3838938f5b5
Yue JIN <yjin@nustarnuclear.com>
2024-05-29 12:17:50 +0800

    chore: up unplugin-vue-router

--- src/plugins/router.ts
+++ src/plugins/router.ts
@@ -1,7 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router/auto'
+import { routes } from 'vue-router/auto-routes'
 import { setupLayouts } from 'virtual:meta-layouts'
 
 export default createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  extendRoutes: (routes) => setupLayouts(routes),
+  routes: setupLayouts(routes),
 })
